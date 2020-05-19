3f8895b12b42783aac7d01b9d4f16195c8b9daf7
madinsane <abrownlow1@gmail.com>
2020-05-19 22:05:16 +0100

    Added Logging

---
+++ Assets/Scripts/Log.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Log : MonoBehaviour
+{
+    private List<string> log;
+    private string displayText;
+    public int maxLines = 50;
+    public Text textBox;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        log = new List<string>();
+        displayText = "";
+        textBox.text = displayText;
+    }
+
+    void Add(string line)
+    {
+        if (log.Count >= maxLines)
+        {
+            log.RemoveAt(0);
+        }
+        log.Add(line);
+        displayText = "";
+        foreach (string entry in log)
+        {
+            displayText += entry;
+        }
+        textBox.text = displayText;
+    }
+}
